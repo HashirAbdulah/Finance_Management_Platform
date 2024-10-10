56188e1084710e8c6f926f2caa382b9b5a99eaef
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-10-10 23:40:42 +0500

    "Updated admin, forms, models, templates, and views for expenses app"

--- expenses/admin.py
+++ expenses/admin.py
@@ -5,4 +5,4 @@ from .models import Expense
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('expense_type', 'amount', 'date', 'employee', 'project')
     search_fields = ('expense_type', 'description')
-    list_filter = ('expense_type', 'date')
+    list_filter = ('expense_type', 'date', 'employee', 'project')
